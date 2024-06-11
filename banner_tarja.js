d9ba02c50408fd76b534345de06388353502ffd4
unknown <gleidistonjunior@gmail.com>
2024-06-11 08:55:34 -0300

    11 06 24

--- banner_tarja.js
+++ banner_tarja.js
@@ -1,25 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
+var bannerPrincipal = document.querySelector('.secao-banners')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="banner_tarja.css">
-</head>
-
-<body>
-
+var codigo = `
     <section id="features-recartes" class="container d-flex">
         <div class="feature-item d-flex">
-            <i class="bi bi-truck"></i>
-            <div>
-                <h4>Frete grátis para todo Brasil</h4>
-                <p>Consulte as condições</p>
-            </div>
+            <a href="https://www.makkers.com.br/pagina/politica-de-frete.html">
+                <i class="bi bi-truck"></i>
+                <div>
+                    <h4>Frete grátis para todo Brasil</h4>
+                    <p>Consulte as condições</p>
+                </div>
+            </a>
         </div>
         <div class="feature-item d-flex">
             <i class="bi bi-credit-card"></i>
@@ -49,9 +39,7 @@
                 <p>para todos os pedidos</p>
             </div>
         </div>
-    </section>
-
-
-</body>
-
-</html>
+    </section>`
+    
+    bannerPrincipal.insertAdjacentHTML('afterend', codigo);
+    
