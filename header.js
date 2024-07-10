9d3d0a70ac021f0b4b026f5f43457667d785ba3f
unknown <gleidistonjunior@gmail.com>
2024-07-10 17:31:45 -0300

    alteração valor de entrega em GYN / Fale conosco no Header

--- header.js
+++ header.js
@@ -1,12 +1,19 @@
-
 function rolagem() {
     var cabecalho = document.getElementById("cabecalho");
-    
+
     if (window.scrollY > 1) { // Verifica se o scroll é maior que 50px
         cabecalho.classList.add('cabecalho-rolagem-makkers');
     } else {
         cabecalho.classList.remove('cabecalho-rolagem-makkers');
     }
 }
-
 window.addEventListener("scroll", rolagem);
+document.addEventListener('DOMContentLoaded', function () {
+    setTimeout(function () {
+
+        var ajudaLiDiv = document.querySelector('li.visible-phone a>div');
+
+        ajudaLiDiv.innerHTML = 'Precisa de ajuda? <br><b>Fale Conosco</b>'
+    
+    }, 700);
+});
